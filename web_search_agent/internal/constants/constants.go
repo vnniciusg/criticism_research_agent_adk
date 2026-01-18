f95f23bcb38297b8d9ae05638d467d721499ac1b
vnnicius <vinnicius109@gmail.com>
2026-01-18 12:40:05 -0300

    feat: add constatnts package with project usefull variables

---
+++ web_search_agent/internal/constants/constants.go
@@ -0,0 +1,16 @@
+package constants
+
+const (
+	AppName   = "criticism_research_agent"
+	ModelName = "gemini-3-flash-preview"
+
+	StateResearchFindings  = "research_findings"
+	StateReviewerFeedback  = "reviewer_feedback"
+	StateTechnicalFindings = "technical_findings"
+	StateEthicalFindings   = "ethical_findings"
+	StateSocialFindings    = "social_findings"
+	StateEconomicFindings  = "economic_findings"
+	StateFinalReport       = "final_report"
+
+	CompletionPhrase = "RESEARCH_COMPLETE"
+)
